cbdda27722ba92df0ab3cc691af174f797262bf0
Rasmus Pehrsson <pehrsson.rasmus@gmail.com>
2023-11-23 14:49:15 +0100

    Added testPayRent()

--- cdio3/src/test/java/com/g_08/MonopolyGameTest.java
+++ cdio3/src/test/java/com/g_08/MonopolyGameTest.java
@@ -32,4 +32,20 @@ public class MonopolyGameTest {
         
     }
 
+    @Test
+    public void testPayRent(){
+        Player owner = new Player("Owner", 10);
+        Player visitor = new Player("Visitor", 10);
+
+        PropertySpace testProperty = new PropertySpace("TestProperty",10,5);
+
+        testProperty.owner=owner;
+
+        testProperty.payRent(visitor);
+
+        assertEquals("Owner should recieve rent",15,owner.money);
+
+        assertEquals("Visitor should pay rent",5,visitor.money);
+    }
+
 }
